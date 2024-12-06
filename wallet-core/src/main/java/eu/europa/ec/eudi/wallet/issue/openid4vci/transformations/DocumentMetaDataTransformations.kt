8cfc1f18ad6457faca6e32d2a1a985638a84cb75
leonidas.papadop <lpapadopoulos@scytales.com>
2024-12-06 17:10:22 +0200

    document metadata model updates + unit tests

--- wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/transformations/DocumentMetaDataTransformations.kt
+++ wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/transformations/DocumentMetaDataTransformations.kt
@@ -7,10 +7,10 @@ import eu.europa.ec.eudi.openid4vci.MsoMdocCredential
 import eu.europa.ec.eudi.openid4vci.SdJwtVcCredential
 import eu.europa.ec.eudi.wallet.document.metadata.DocumentMetaData
 
-fun CredentialConfiguration.extractDocumentMetaData(): DocumentMetaData? {
+fun CredentialConfiguration.extractDocumentMetaData(): DocumentMetaData {
     val documentDisplay = display.map { it.toDocumentDisplay() }
 
-    val claims: Map<DocumentMetaData.ClaimName, DocumentMetaData.Claim>? = when (this) {
+    val claims = when (this) {
         is MsoMdocCredential -> claims.fromMsoDocToDocumentClaim()
         is SdJwtVcCredential -> claims.fromSdJwtVToDocumentClaim()
         else -> null
@@ -35,33 +35,29 @@ private fun Display.Logo.toDocumentLogo():
         DocumentMetaData.Display.Logo =
     DocumentMetaData.Display.Logo(uri, alternativeText)
 
-private fun Map<String, Map<String, Claim>>.fromMsoDocToDocumentClaim(): Map<DocumentMetaData.ClaimName, DocumentMetaData.Claim> {
+private fun Map<String, Map<String, Claim>>.fromMsoDocToDocumentClaim(): List<DocumentMetaData.Claim> {
 
     return this.flatMap { (namespace, claimsMap) ->
         claimsMap.mapNotNull { (name, claim) ->
-
-            val claimName = DocumentMetaData.MsoMdocClaimName(
+            val claimName = DocumentMetaData.Claim.Name.MsoMdoc(
                 name = name,
                 nameSpace = namespace
             )
-            val documentClaim = claim.fromMsoDocToDocumentClaim()
-            claimName to documentClaim
+            claim.fromMsoDocToDocumentClaim(claimName)
         }
-    }.toMap()
+    }
 }
 
-private fun Map<String, Claim?>?.fromSdJwtVToDocumentClaim(): Map<DocumentMetaData.ClaimName, DocumentMetaData.Claim>? {
-    if (this == null) return null
-
-    return mapNotNull { (name, claim) ->
-        val claimName = DocumentMetaData.SdJwtVcsClaimName(name = name)
-        val documentClaim = claim.fromMsoDocToDocumentClaim()
-        claimName to documentClaim
-    }.toMap()
-
+private fun Map<String, Claim?>?.fromSdJwtVToDocumentClaim(): List<DocumentMetaData.Claim>? {
+    return this?.mapNotNull { (name, claim) ->
+        val claimName = DocumentMetaData.Claim.Name.SdJwtVc(name = name)
+        claim.fromMsoDocToDocumentClaim(claimName)
+    }
 }
 
-private fun Claim?.fromMsoDocToDocumentClaim(): DocumentMetaData.Claim = DocumentMetaData.Claim(
+private fun Claim?.fromMsoDocToDocumentClaim(name: DocumentMetaData.Claim.Name): DocumentMetaData.Claim =
+    DocumentMetaData.Claim(
+        name = name,
     mandatory = this?.mandatory,
     valueType = this?.valueType,
     display = this?.display?.map { display ->
