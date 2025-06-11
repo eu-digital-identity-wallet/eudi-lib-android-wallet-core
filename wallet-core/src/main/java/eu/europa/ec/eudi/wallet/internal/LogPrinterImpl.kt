eb06a9835d3281b216871d3156b03c46d724310c
vkanellopoulos <vkanellopoulos@scytales.com>
2025-06-11 13:26:38 +0300

    bump multipaz version to 0.92.0; refactor tests to use mock logging;Refactor OpenId4VpUtils using CredentialIssuedData; Refactor building SecureAreaRepository in EudiWallet.Builder

--- wallet-core/src/main/java/eu/europa/ec/eudi/wallet/internal/LogPrinterImpl.kt
+++ wallet-core/src/main/java/eu/europa/ec/eudi/wallet/internal/LogPrinterImpl.kt
@@ -1,12 +1,12 @@
 /*
  * Copyright (c) 2024-2025 European Commission
- *
+ *  
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *  
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,12 +20,12 @@ import eu.europa.ec.eudi.wallet.logging.Logger
 import org.multipaz.util.Logger as IdentityLogger
 
 internal class LogPrinterImpl(private val logger: Logger) : IdentityLogger.LogPrinter {
-    override fun printLn(level: Int, tag: String, msg: String, throwable: Throwable?) {
+    override fun print(level: IdentityLogger.LogPrinter.Level, tag: String, msg: String, throwable: Throwable?) {
         val coreLevel = when (level) {
-            IdentityLogger.LEVEL_W -> Logger.LEVEL_INFO
-            IdentityLogger.LEVEL_E -> Logger.LEVEL_ERROR
-            IdentityLogger.LEVEL_I -> Logger.LEVEL_INFO
-            IdentityLogger.LEVEL_D -> Logger.LEVEL_DEBUG
+            IdentityLogger.LogPrinter.Level.WARNING -> Logger.LEVEL_INFO
+            IdentityLogger.LogPrinter.Level.ERROR -> Logger.LEVEL_ERROR
+            IdentityLogger.LogPrinter.Level.INFO -> Logger.LEVEL_INFO
+            IdentityLogger.LogPrinter.Level.DEBUG -> Logger.LEVEL_DEBUG
             else -> Logger.LEVEL_INFO
         }
         val record = Logger.Record(
