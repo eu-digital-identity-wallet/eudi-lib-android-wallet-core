c54ba8f7d9299d9e0cbc3a2c613129db4b324eaa
vkanellopoulos <vkanellopoulos@scytales.com>
2025-10-13 10:30:14 +0300

    wip: fix parsing jwk for proof signers

--- wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/DPoPSigner.kt
+++ wallet-core/src/main/java/eu/europa/ec/eudi/wallet/issue/openid4vci/DPoPSigner.kt
@@ -34,7 +34,7 @@ class DPoPSigner private constructor(val algorithm: Algorithm) : Signer<JWK> {
         ?: throw IllegalArgumentException("Unsupported algorithm: $algorithm")
 
     override suspend fun acquire(): SignOperation<JWK> {
-        val jwk = JWK.parse(privateKey.publicKey.toJwk())
+        val jwk = JWK.parse(privateKey.publicKey.toJwk().toString())
         return SignOperation(
             function = { input -> Crypto.sign(privateKey, algorithm, input).toDerEncoded() },
             publicMaterial = jwk
