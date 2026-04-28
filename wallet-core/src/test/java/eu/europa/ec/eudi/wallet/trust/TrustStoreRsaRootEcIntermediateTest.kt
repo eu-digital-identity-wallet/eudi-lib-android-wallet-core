9b68431fc9d412ffddfb81851091ab41fed7c383
Stefanos Raptis <sraptis@scytales.com>
2026-04-28 17:33:19 +0300

    Remove CRL mock from TrustStoreRsaRootEcIntermediateTest CertificateCRLValidation was removed in iso18013-data-transfer 0.13.0. The default RevocationPolicy.NoCheck makes the mock unnecessary

--- wallet-core/src/test/java/eu/europa/ec/eudi/wallet/trust/TrustStoreRsaRootEcIntermediateTest.kt
+++ wallet-core/src/test/java/eu/europa/ec/eudi/wallet/trust/TrustStoreRsaRootEcIntermediateTest.kt
@@ -1,44 +1,17 @@
 package eu.europa.ec.eudi.wallet.trust
 
 import eu.europa.ec.eudi.iso18013.transfer.readerauth.ReaderTrustStoreImpl
-import io.mockk.every
-import io.mockk.mockkStatic
-import io.mockk.unmockkStatic
 import org.bouncycastle.jce.provider.BouncyCastleProvider
-import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import java.security.Security
-import java.security.cert.X509Certificate
 import kotlin.test.assertTrue
 
 class TrustStoreRsaRootEcIntermediateTest {
 
-    // Fully qualified name of the internal class we need to mock
-    private val crlClassName = "eu.europa.ec.eudi.iso18013.transfer.internal.readerauth.crl.CertificateCRLValidation"
-
     @Before
     fun setup() {
         Security.addProvider(BouncyCastleProvider())
-
-        // Mock the internal class by Name
-        mockkStatic(crlClassName)
-
-        // Stub the 'verify' method to do nothing
-        val crlClass = Class.forName(crlClassName)
-        val verifyMethod = crlClass.getMethod("verify", X509Certificate::class.java)
-
-
-        // Define the stub
-        every {
-            verifyMethod.invoke(null, any<X509Certificate>())
-        } returns Unit
-    }
-
-    @After
-    fun tearDown() {
-        // Clean up the static mock to avoid affecting other tests
-        unmockkStatic(crlClassName)
     }
 
     @Test
@@ -51,7 +24,7 @@ class TrustStoreRsaRootEcIntermediateTest {
             profileValidation = { _, _ -> true }
         )
 
-        val result = trustStore!!.validateCertificationTrustPath(certificateChain)
+        val result = trustStore.validateCertificationTrustPath(certificateChain)
 
         assertTrue(result, "Trust store should accept RSA Root -> EC Intermediate -> EC Leaf chain")
     }
